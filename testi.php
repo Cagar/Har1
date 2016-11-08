9cdea58b18af12cbf20aea0226c89a486530c8e3
Eetu Lehmus <t4leee01@students.oamk.fi>
2016-11-08 09:12:10 +0200

    TESTITITITIOTITI
    Joo tehtiin vähän testiä

---
+++ testi.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Testataan harjia ooooo</title>
+</head>
+<body>
+
+<h1>Suurimman tason hotsiksksosko</h1>
+<p>tässä tekstiä haluaisin vain keksiä </p>
+<?php
+$x=5;
+echo 'Muumimamman arvo on '.$x;
+echo '<br>';
+echo "Muuttujan arvo on $x";
+?>
+
+</body>
+</html>
